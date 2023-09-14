764c3f636b6ed27a4d201badfbcfbe927c7a18a2
Thinh Tran <tranthinhqnam@gmail.com>
2023-09-14 16:47:01 +0700

    Update TC_05

--- selenium/webdriver/Topic_04_Xpath_Css_I.java
+++ selenium/webdriver/Topic_04_Xpath_Css_I.java
@@ -4,16 +4,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class Topic_04_Xpath_Css {
+public class Topic_04_Xpath_Css_I {
 
     WebDriver driver;
 
@@ -24,6 +22,7 @@ public class Topic_04_Xpath_Css {
 
         driver.get("http://live.techpanda.org/index.php/customer/account/login/");
     }
+
     @Test
     public void TC_01_Login_Empty_Email_And_Password() {
 
@@ -63,6 +62,7 @@ public class Topic_04_Xpath_Css {
 
     @Test
     public void TC_04_Login_Incorrect_Email() {
+        driver.navigate().refresh();
         driver.findElement(By.id("email")).sendKeys("tranthinhqnam@gmail.com");
         driver.findElement(By.name("login[password]")).sendKeys("1234567");
         //Click button
@@ -70,6 +70,35 @@ public class Topic_04_Xpath_Css {
         Assert.assertEquals(driver.findElement(By.xpath("//li[@class='error-msg']//span")).getText(), "Invalid login or password.");
 
     }
+
+    @Test
+    public void TC_05_Create_New_Account() {
+
+        //register
+
+        driver.get("http://live.techpanda.org/index.php/");
+        driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account'][normalize-space()='My Account']")).click();
+        driver.findElement(By.xpath("//a[@title='Create an Account']")).click();
+        driver.findElement(By.id("firstname")).sendKeys("Tran");
+        driver.findElement(By.id("middlename")).sendKeys("Van");
+        driver.findElement(By.id("lastname")).sendKeys("Thinh");
+        driver.findElement(By.id("email_address")).sendKeys("t11111ranthinhqnam@gmail.com");
+        driver.findElement(By.id("password")).sendKeys("030401T");
+        driver.findElement(By.id("confirmation")).sendKeys("030401T");
+        driver.findElement(By.id("is_subscribed")).click();
+        driver.findElement(By.xpath("//button[@title='Register']")).click();
+        //verify the name displays on dashboard
+        WebElement element =driver.findElement(By.xpath("//div[@class='box-content']//p[contains(text(),'Tran Van Thinh')]"));
+        String actualText = element.getText();
+        Assert.assertEquals(actualText,"Tran Van Thinh\n" +
+                "t11111ranthinhqnam@gmail.com\n" +
+                "Change Password");
+        driver.findElement(By.xpath("//a[@class='skip-link skip-account']")).click();
+        driver.findElement(By.xpath("//a[@title='Log Out']")).click();
+        Assert.assertEquals(driver.findElement(By.xpath("//div[@class='page-title']//h1")).getText(),"YOU ARE NOW LOGGED OUT");
+    }
+
+
     @AfterClass
     public void afterClass() {
         driver.quit();
